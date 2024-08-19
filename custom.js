7e78717c459a582f9cd447bcccc17acdbf8cce96
Mouradmore <mouradx524@gmail.com>
2024-08-19 14:07:10 +0100

    Update and rename index.js to custom.js

---
+++ custom.js
@@ -0,0 +1,448 @@
+window.addEventListener('load', ()=>{
+    document.querySelector('.preloader_wrapper').classList.add("preloading")
+})
+
+
+let video_details = [
+
+    [
+    "اهم الجمل في اللغة الانجليزية تعلم اللغة الانجليزية من الصفر",
+    "111",
+    "video",
+    "2024",
+    // Not Requared "https://drive.google.com/file/d/1Z4LIiQsux5uu1Dhi3Q3ClbjXilSqOFzL/preview"
+    "01.mp4"
+    ],
+
+
+    [
+    "اهم الجمل في اللغة الانجليزية تعلم اللغة الانجليزية من الصفر",
+    "02",
+    "131K",
+    "3 days",
+    // Not Requared "https://drive.google.com/file/d/1OdX05VtfRFyAr-1pDyqgiSUIyMkVOns0/preview"
+    "02.mp4"
+],
+
+
+    [
+    "اهم الجمل والعبارات الاساسية لتعلم اللغة الانجليزية تعلم اللغة الانجليزية من الصفر",
+    "03",
+    "1.8 lakh",
+    "5 months",
+    // Not Requared "https://drive.google.com/file/d/1pWeQ--wEYUHdOvwhsnlrCW0RWj_hADDS/preview"
+    "03.mp4"
+],
+
+
+    [
+    "اهم الجمل والعبارات الاساسية لتعلم اللغة الانجليزية تعلم اللغة الانجليزية من الصفر",
+    "03",
+    "3.4M",
+    "1 years",
+    // Not Requared "https://drive.google.com/file/d/1Z2XerGXVELE_IPD-9rRhZwXlCkkYGno3/preview"
+    "04.mp4"
+],
+
+
+    [
+    "اهم الجمل والعبارات الاساسية لتعلم اللغة الانجليزية تعلم اللغة الانجليزية من الصفر",
+    "03",
+    "229K",
+    "2 years",
+    // Not Requared "https://drive.google.com/file/d/1bbdwLGSKpIHe_CL-HODbQy-MvgNpffHw/preview"
+    "05.mp4"
+],
+
+
+    [
+    "تعلم اللغة الإنجليزية من الصفر  باللهجة البريطانية ",
+    "04",
+    "668K",
+    "4 hours",
+    // Not Requared "https://drive.google.com/file/d/1VL0NPlDgRjADZs0zWrLotFouNE9z5vpn/preview"
+    "06.mp4"
+],
+
+
+    [
+    "تعلم اللغة الإنجليزية من الصفر  باللهجة البريطانية",
+    "04",
+    "39K",
+    "4 months",
+    // Not Requared "https://drive.google.com/file/d/1D_2402oD4lDQ8-B8SG8M_OgJ_bHYZPsS/preview"
+    "07.mp4"
+],
+
+
+    [
+    "كيف تترجم أي جملة  ؟؟",
+    "05",
+    "4M",
+    "2 years",
+    // Not Requared "https://drive.google.com/file/d/1YrIJfPperAtum5N3em5XnWPDcLSvaxke/preview"
+    "001.mp4"
+],
+
+
+    [
+    "أكثر 50 صفة في اللغة الإنجليزية   صوت وصورة",
+    "06",
+    "844K",
+    "2 weeks",
+    // Not Requared "https://drive.google.com/file/d/1Ck8Sn5zabbYj0GdmWZU4JmFxEGtNE8fl/preview"
+    "002.mp4"
+],
+
+
+    [
+    "كورس أهم 100 سؤال وجواب في اللغة الانجليزية",
+    "07",
+    "79.6K",
+    "4 months",
+    // Not Requared "https://drive.google.com/file/d/1o2odlDyDBm10-ZZbF6QNc5LguaFjFxSD/preview"
+    "08.mp4"
+],
+
+
+    [
+    "كورس أهم 100 سؤال وجواب في اللغة الانجليزية",
+    "07",
+    "89.6K",
+    "3 months",
+    // Not Requared "https://drive.google.com/file/d/1d8YWDOFrXyoUw6estGdvgztopb2AE0qt/preview"
+    "09.mp4"
+],
+
+
+    [
+    "كورس أهم 100 سؤال وجواب في اللغة الانجليزية",
+    "07",
+    "2.6M",
+    "8 months",
+    // Not Requared "https://drive.google.com/file/d/18OrsRLLGUiP3FP8-H7LTrplcm3arpLnk/preview"
+    "10.mp4"
+],
+
+
+    [
+    "تعلم اللغة الانجليزية _اهم الافعال في اللغة الانجليزية. مع الترجمة",
+    "08",
+    "359K",
+    "6 months",
+    // Not Requared "https://drive.google.com/file/d/1FzC5_FmEGY_TfwdzxKKM2LLX_DDSvCB_/preview"
+    "003.mp4"
+],
+
+
+    [
+    "تعلم اهم العبارات الاساسية واكثرها استخداما في الانجليزية- الدرس الاول",
+    "09",
+    "173K",
+    "22 hours",
+    // Not Requared "https://drive.google.com/file/d/1cFMHkox0ZRKF10xVmoDiASuIJQ1yOjtI/preview"
+    "004.mp4"
+],
+
+
+    [
+    "تعلم اللغة الإنجليزية من الصفر",
+    "11",
+    "582K",
+    "9 months",
+    // Not Requared "https://drive.google.com/file/d/1ef9xNHsNPzrb3lhoBc5QG1fJrw4o9zQi/preview"
+    "005.mp4"
+],
+
+
+    [
+    "اهم الجمل والعبارات الاساسية في تعلم اللغة الانجليزية من الصفر بسهولة",
+    "01",
+    "3.7M",
+    "7 days",
+    // Not Requared "https://drive.google.com/file/d/1Z4LIiQsux5uu1Dhi3Q3ClbjXilSqOFzL/preview"
+    "11.mp4"
+],
+    
+
+    [
+    "اهم الجمل والعبارات الاساسية في تعلم اللغة الانجليزية من الصفر بسهولة",
+    "12",
+    "37K",
+    "12 days",
+    // Not Requared "https://drive.google.com/file/d/1qdOdoHM2l133TlrIGbdxr0BuseQ8pzhn/preview"
+    "12.mp4"
+    ],
+    
+        
+    [
+    "اهم الجمل والعبارات الاساسية في تعلم اللغة الانجليزية من الصفر بسهولة",
+    "12",
+    "473K",
+    "8 months",
+    // Not Requared "https://drive.google.com/file/d/1zAvQhkHeRqZwi4O7qSUTqH1J2c3h6XNh/preview"
+    "13.mp4"
+    ]
+]
+
+
+for (let index = 0; index < video_details.length; index++) {
+    let element = document.querySelector(".content_part")
+
+    element.innerHTML += `
+    
+<div class='thumb_vid_wrapper'>
+<div class='thumb_wrapper'>
+<img class="thumbnail" src="${video_details[index][1]}.jpg">
+    </div>
+<div class='title_wrapper'>
+<div class='right_title'>
+<div class='right_top_title'> ${video_details[index][0]}</div>
+<div class='right_chnl_title'>
+<div class='chnl_name'>
+<h6> ${video_details[index][1]}</h6>
+<i class='fas fa-check-circle'></i>
+                </div>
+<div class='right_time_title'><span>${video_details[index][2]}</span> 
+<span class="point" style ="font-size: 4px !important; padding: 0px 5px;"><i class='fas fa-circle'></i></span> <span>${video_details[index][3]} </span></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    `
+}
+
+for (let i = 0; i < 9; i++) {
+    let braille= document.querySelector(".braille")
+
+    braille.innerHTML += `
+        <div class="squre_braille"></div>
+    `
+}
+
+
+let content_part = document.querySelector(".content_part")
+let yt_home_logo = document.getElementById("youtube_home_logo")
+let video_thumb = document.querySelectorAll(".thumb_vid_wrapper")
+let left_menu_wrapper = document.querySelector(".left_menu_wrapper")
+let tab_title = document.querySelector("#tab_title")
+let play_part = document.querySelector(".play_part")
+let main_part = document.querySelector(".main_part")
+let back_btn = document.querySelector(".back_btn")
+let header_bar = document.querySelector(".header_bar")
+let bars_div = document.querySelector(".bars_div")
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+video_thumb.forEach( (number,i) => {
+
+    number.addEventListener('click', ()=> {
+        tab_title.innerHTML = video_details[i][0]
+        topFunction()
+        left_menu_wrapper.style.display = "none"
+        main_part.setAttribute("style","padding: 25px 40px;")
+        content_part.classList.add("content_when_played")
+        header_bar.style.display = "none";
+        back_btn.style.display = "inline-block";
+        bars_div.style.paddingTop = "7px";
+        
+        play_part.innerHTML = `
+<div class="play_page_container">
+<div class="play_page_left">
+<div class="play_video_wrapper">
+<video onclick="PlayPause()" id="videos" src="${video_details[i][4]}" allowfullscreen autoplay></video>
+<div onclick="PlayPause()" class="overly_poster">
+<i id="playPause_btn" class="fas fa-pause"></i></div>
+
+<div class="range_wrapper">
+<div class="inp_range">
+<input value="0" min="0" max="100" type="range" name="range" id="rangeId" step="1"></div>
+                
+<div class="dur_play_wrapper">
+                    
+<div><i onclick="PlayPause()" id="playPause_btn2" class="fas fa-pause"></i></div>
+            
+<div class="duration_wrapper">
+<div class="left_duration">
+<div class="minute">0</div>
+<div>:</div>
+<div class="second">0</div>
+           </div>
+           <p>/</p>
+<div class="right_duration">
+<div class="minute_duration">0</div>
+<div>:</div>
+<div class="second_duration">0</div>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+
+
+        </div>
+<div class="video_title">
+<div class="video_heading"><h3>${video_details[i][0]}</h3></div>
+<div class="video_options_wrapper">
+    
+
+    </div>
+    </div>
+    
+<div class="channel_details">
+<div class="vid_chnl_wrapper">
+
+<div class="chnl_name_video_play_wrapper">
+<div class='chnl_name chnl_name_video_play'>
+<h6> ${video_details[i][1]}</h6>
+<i class='fas fa-check-circle'></i>
+       </div>
+    
+ </div>
+    </div>
+<div class="subscribe_wrapper">
+ <h1>InTexT English</h1>
+    </div></div>
+    
+        </div>
+      
+    </div>  `
+
+
+let VideoTag = document.getElementById("videos")
+let range = document.querySelector("#rangeId");
+let minute = document.querySelector(".minute");
+let minute_duration = document.querySelector(".minute_duration");
+let second = document.querySelector(".second");
+let second_duration = document.querySelector(".second_duration");
+let play_btn = document.querySelector("#playPause_btn")
+let play_btn2 = document.querySelector("#playPause_btn2")
+
+range.addEventListener("input", VidSeek);
+VideoTag.addEventListener("timeupdate", seektimeupdate)
+
+
+function seektimeupdate() {
+    let new_time = VideoTag.currentTime * (100/VideoTag.duration)
+    range.value = new_time;
+
+    minute.innerHTML = (VideoTag.currentTime/60);
+    minute_duration.innerHTML = (VideoTag.duration/60);
+
+    if ((VideoTag.currentTime%60)<10) {
+        second.innerHTML = "0" + (VideoTag.currentTime%60);
+    } else{
+        second.innerHTML = (VideoTag.currentTime%60);
+    }
+
+    if ((VideoTag.duration%60)<10) {
+        second_duration.innerHTML = "0" + (VideoTag.duration%60);
+    } else{
+        second_duration.innerHTML = (VideoTag.duration%60);
+    }
+        
+    if (VideoTag.currentTime == VideoTag.duration) {
+        play_btn.classList.add("fa-play");
+        play_btn.classList.remove("fa-pause");
+        play_btn2.classList.add("fa-play");
+        play_btn2.classList.remove("fa-pause");
+        play_btn.setAttribute("style", "transform: translateY(0px); transition:.2s; opacity: 1;")
+    }
+
+}
+
+
+function VidSeek() {
+    let seekto = VideoTag.duration * (range.value/100)
+    VideoTag.currentTime = seekto;
+}
+
+
+
+        let like = document.querySelector("#like")
+        let unlike = document.querySelector("#unlike")
+
+        let liked = false;
+        let unliked = false;
+        
+        let like_link = "https://img.icons8.com/material-sharp/24/000000/thumb-up.png"
+        let removed_like_link = "https://img.icons8.com/material-outlined/24/000000/thumb-up.png"
+        
+        let unlike_link = "https://img.icons8.com/material-sharp/24/000000/thumbs-down.png"
+        let removed_unlike_link = "https://img.icons8.com/material-outlined/24/000000/thumbs-down.png"
+
+    
+        like.addEventListener("click", function() {
+
+            if (!liked) {
+                liked = true
+                unliked = false
+                like.src = like_link;
+                unlike.src = removed_unlike_link;
+            } else if (liked) {
+                liked = false
+                unliked = true
+                like.src = removed_like_link;
+            }
+
+        });
+        
+
+        unlike.addEventListener("click", function() {
+
+            if (!unliked) {
+                unliked = true
+                liked = false
+                unlike.src = unlike_link;
+                like.src = removed_like_link;
+            } else if (unliked) {
+                unliked = false
+                liked = true
+                unlike.src = removed_unlike_link;
+            }
+
+        });
+        
+
+
+    })
+
+})
+
+
+function PlayPause() {
+    let VideoTag = document.querySelector("#videos")
+    let play_btn = document.querySelector("#playPause_btn")
+    let play_btn2 = document.querySelector("#playPause_btn2")
+    let overly_poster = document.querySelector(".overly_poster")
+    let range_wrapper = document.querySelector(".range_wrapper")
+
+    overly_poster.classList.toggle('overlys')
+    range_wrapper.classList.toggle('overlys')
+
+    if(VideoTag.paused) {
+        VideoTag.play()
+        play_btn.classList.add("fa-pause");
+        play_btn.classList.remove("fa-play");
+        play_btn2.classList.add("fa-pause");
+        play_btn2.classList.remove("fa-play");
+        play_btn.setAttribute("style", "transform: translateY(50px); transition:.2s;opacity:0")
+
+    } else if (VideoTag.played) {
+        VideoTag.pause()
+        play_btn.classList.add("fa-play");
+        play_btn.classList.remove("fa-pause");
+        play_btn2.classList.add("fa-play");
+        play_btn2.classList.remove("fa-pause");
+        play_btn.setAttribute("style", "transform: translateY(50px); transition:.2s; opacity: 0;")
+
+    }
+
+}
